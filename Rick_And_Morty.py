e6ba1edb1cda039abc73b9b2e9c9ab3d6b92efb8
Ian R. Taylor <iantay@upenn.edu>
2017-01-30 16:49:41 -0500

    Create a super simple prototype

--- Rick_And_Morty.py
+++ Rick_And_Morty.py
@@ -1,24 +1,100 @@
 #!/usr/bin/env python
 
+import sys
+
 import curses
+from curses import wrapper
+import textwrap
 
-window = curses.initscr()
-window.immedok(True)
+# cmd.Cmd
+from cmd import Cmd
 
-try:
-    window.border(0)
+# JSON support
+import json
+from pprint import pprint
 
-    (height, width) = window.getmaxyx()
+demo_room_path = './data/demo.json'
+prompt_enter = "Press ENTER to continue..."
 
-    menuwin = curses.newwin(6, width - 12, height - 8, 5)
-    menuwin.immedok(True)
 
-    menuwin.box()
-    #menuwin.addstr("Hello World of Curses!")
+class FakeStdIO(object):
+    """
+        A class to override write and readline methods for stdout and stdin respectively.
+        This class utilizes a curses window to format text, wrapping it appropriately.
+    """
+    def __init__(self,stdscr):
+        self.stdscr = stdscr
 
-    window.getch()
+    def write(self, str):
+        height,width = self.stdscr.getmaxyx()
+        if len(str) >= width:
+            for line in textwrap.wrap(str,width-2):
+                self.stdscr.addstr(line)
+                self.stdscr.addstr("\n")
+        else:
+            self.stdscr.addstr(str)
+        self.stdscr.refresh()
 
-except KeyboardInterrupt:
-    pass
-finally:
-    curses.endwin()
+    def readline(self):
+        temp = self.stdscr.getstr()
+        if len(temp)==0:
+            temp = ' '
+        return temp
+
+
+
+class MyPrompt(Cmd):
+
+    prompt = '>> '
+    def do_hello(self, args):
+        """Says hello. If you provide a name, it will greet you with it."""
+        if len(args) == 0:
+            name = 'stranger'
+        else:
+            name = args
+        print "Hello, %s" % name
+
+    def do_quit(self, args):
+        """Quits the program."""
+        print "Quitting."
+        raise SystemExit
+
+
+def main():
+    stdscr = curses.initscr()
+    curses.cbreak()
+    #curses.noecho()
+    curses.echo()
+    stdscr.keypad(1)
+    stdscr.scrollok(True)
+    stdscr.clear()
+    stdscr.refresh()
+
+    try:
+        io = FakeStdIO(stdscr)
+        sys.stdin = io
+        sys.stdout = io
+
+        helloparser = MyPrompt()
+        helloparser.stdout = io
+        helloparser.stdin = io
+
+        # Print current room and prompt
+        with open(demo_room_path) as json_data:
+            data = json.load(json_data)
+            #print(d)
+            sys.stdout.write(data["name"] + '\n')
+            sys.stdout.write(data["longform"] + '\n')
+
+        helloparser.cmdloop()
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        curses.nocbreak()
+        stdscr.keypad(0)
+        curses.echo()
+        curses.endwin()
+
+if __name__ == '__main__':
+    main()
