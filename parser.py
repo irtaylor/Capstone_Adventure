c4f507225aab41c0cbb1a3e12653ae0055b9b192
Stephanie Creamer <creamers@oregonstate.edu>
2017-03-09 19:08:42 -0800

    fixed duplicate 'away we gooo' message

--- parser.py
+++ parser.py
@@ -295,7 +295,6 @@ class CommandParser(Cmd):
                         new_world = text_helpers.convert_to_key(stripped)
                         self.player.set_current_world(self.worlds[new_world])
                         self.items["portal_gun"].num_uses -= 1
-                        print self.items["portal_gun"].success_message
                     # gun is out of juice, return error text
                     else:
                         print self.items["portal_gun"].get_cannot_use_description()
