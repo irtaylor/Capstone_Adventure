caf8aa8660c57abc5a0d6aea40bb8ea0118cac26
Kyle <kylejbergman@gmail.com>
2017-03-12 14:56:05 -0700

    added ray gun ending check

--- gameover.py
+++ gameover.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import parser
+
 
 def check_for_ending(parser):
     """
@@ -9,11 +9,14 @@ def check_for_ending(parser):
         ending = 'win'
     elif check_stranded(parser):
         ending = 'stranded'
-    elif check_death(parser):
+    elif check_death_gravity_boots(parser):
+        ending = 'death'
+    elif check_death_ray_gun(parser):
         ending = 'death'
     else:
         ending = ''
     return ending
+
 
 def check_win(parser):
     win = False
@@ -21,9 +24,10 @@ def check_win(parser):
         win = True
     return win
 
+
 def check_stranded(parser):
     stranded = True
-    if (parser.items["portal_gun"].num_uses == 0):      # if portal gun charge done, we might be stranded
+    if parser.items["portal_gun"].num_uses == 0:      # if portal gun charge done, we might be stranded
         for key in parser.player.current_world.rooms.keys():
             room = parser.player.current_world.rooms[key]
 
@@ -44,10 +48,19 @@ def check_stranded(parser):
         stranded = False
     return stranded
 
-def check_death(parser):
+
+def check_death_gravity_boots(parser):
     death = False
-    if (parser.lastcmd == 'use gravity boots' or parser.lastcmd == 'wear gravity boots' \
-        or parser.lastcmd == 'equip gravity boots' or parser.lastcmd == 'strap gravity boots') \
-        and (parser.items["gravity_boots"].num_uses <= 0):
+    if (parser.lastcmd == 'use gravity boots' or parser.lastcmd == 'wear gravity boots'
+        or parser.lastcmd == 'equip gravity boots' or parser.lastcmd == 'strap gravity boots') and \
+            (parser.items["gravity_boots"].num_uses <= 0):
+        death = True
+    return death
+
+
+def check_death_ray_gun(parser):
+    death = False
+    if (parser.lastcmd == 'use ray gun' or parser.lastcmd == 'shoot ray gun'
+            or parser.lastcmd == 'fire ray gun') and parser.current_room.name.lower() == "prison entrance":
         death = True
     return death
