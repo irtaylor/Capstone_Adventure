407ac7776202d782823d84b0a4761b229c5f553f
Ian R. Taylor <iantay@upenn.edu>
2017-03-12 13:43:54 -0400

    Define win condition

--- gameover.py
+++ gameover.py
@@ -2,9 +2,11 @@
 import parser
 
 def check_for_ending(parser):
+    """
+        Check the defined game ending conditions, and call the appropriate ending.
+    """
     if check_win(parser):
-        #ending = 'win'
-        ending = ''
+        ending = 'win'
     elif check_stranded(parser):
         ending = 'stranded'
     elif check_death(parser):
@@ -15,7 +17,7 @@ def check_for_ending(parser):
 
 def check_win(parser):
     win = False
-    if parser.player.current_room.name == "Summer's Cell":
+    if (parser.player.current_room.name == "Summer's Cell") and ("win_flag" in parser.player.current_room.items):
         win = True
     return win
 
@@ -26,15 +28,15 @@ def check_stranded(parser):
             room = parser.player.current_world.rooms[key]
 
             for item_key in room.items:
-                if item_key == 'battery':
+                if item_key == 'multiverse_battery':
                     stranded = False
                     break
             for hidden_item_key in room.hidden_items:
-                if hidden_item_key == 'battery':
+                if hidden_item_key == 'multiverse_battery':
                     stranded = False
                     break
             for item_key in parser.player.inventory:
-                if item_key == 'battery':
+                if item_key == 'multiverse_battery':
                     stranded = False
                     break
 
